bac12afbf6839634923ceed7a6061cc1d75e7500
TimaDootaliev <timur.dootaliev.td@gmail.com>
2023-03-01 20:03:18 +0600

    post crud

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from app.models.posts.post_model import Genre, PostGenres, Post
 from app.models.basemodel import db_connection, db
 from app.queries.genres import create_genre, delete_genre, get_genres
+from app.queries.posts import create_post, get_all_films, get_film_by_id
 
 @db
 def create_tables():
@@ -11,4 +12,18 @@ create_tables()
 create_genre('Детектив')
 create_genre('Ужасы')
 # delete_genre('Детектив')
-print(get_genres())
+# print(get_genres())
+# create_post(
+#     'Ходячие мертвецы', 
+#     "Сериал про зомби!",
+#     '2003',
+#     'USA',
+#     ['Детектив', 'Ужасы'])
+# create_post(
+#     'Шерлок Холмс', 
+#     "Сериал про детектива!",
+#     '2005',
+#     'USA',
+#     ['Детектив'])
+# print(get_all_films())
+print(get_film_by_id(1))
